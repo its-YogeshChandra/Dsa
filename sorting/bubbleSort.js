e5e6b299ba2073a08abcc60a442d44b5b65dff1e
its-YogeshChandra <pandittheroyal@gmail.com>
2025-11-18 20:03:16 +0530

    added sorting

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,20 @@
+let ar = [5, 2, 8, 1, 3];
+
+const bubblesort = () => {
+  //fix the array
+  let i = 0;
+  let j = i + 1;
+
+
+  //fix the array
+  if (arr[i] < arr[j]) {
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+  i++
+  j++
+
+}
+
+cosnole.log(bubblesort())
